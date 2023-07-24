c3ad5b13df40ba0d65fe2fda12f20445e4c7141d
tomitheninja <sudi.tamas@simonyi.bme.hu>
2023-07-24 18:23:25 +0200

    Add static-serve support to backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRootAsync({
+      useFactory: () => {
+        const rootPath = process.env['PEK_CLIENT_PATH'];
+        if (!rootPath) return [];
+        return [{ rootPath }];
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
